437d7f176795c887fa6cdbe0d16b31b8821a2cf8
Precious <99884472+pdramil@users.noreply.github.com>
2022-03-03 11:06:29 +0800

    Add files via upload

---
+++ Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(void){
+
+    int x, y, difference;
+
+    x = 87;
+    y = 15;
+    difference = x - y;
+
+    printf("The difference of %d minus %d is %d.", x, y, difference);
+
+    return 0;
+}
